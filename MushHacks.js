e08d0b36118c9d11dd77e4e22e54ec534db9f05e
insoln <insoln@ya.ru>
2015-03-08 22:34:12 +0100

    Create MushHacks.js

---
+++ MushHacks.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name       MushHacks
+// @version    0.01
+// @grant      GM_xmlhttpRequest
+// @grant      GM_addStyle
+// @match      http://mush.twinoid.com/*
+// @match      http://mush.twinoid.com/#
+// ==/UserScript==
+// jshint multistr: true
+// @require http://code.jquery.com/jquery-latest.js
+
+
+GM_addStyle('\
+.tid_editorContent {visibility: visible}\
+}
