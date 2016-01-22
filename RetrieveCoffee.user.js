7babb3305c982efb53ca32218f11025f09b91a36
insoln <insoln@users.noreply.github.com>
2016-01-22 01:10:23 +0100

    Update RetrieveCoffee.user.js

--- RetrieveCoffee.user.js
+++ RetrieveCoffee.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         Кофедоставатель
-// @version      0.4
-// @description  Дежурный по эскалатору справок не дает!
+// @name         Mush: 16 coffees, please!
+// @version      0.5
+// @description  Automatically retrieves coffee on cycle change.
 // @author       Innokentiy
 // @match        http://mush.twinoid.com/
 // @downloadURL  https://raw.githubusercontent.com/insoln/mush/master/RetrieveCoffee.user.js
@@ -12,12 +12,28 @@
 
 function RetrieveCoffee() {
     if ($('#input').attr('d_name')=='Refectory') {
-        if ($('#timetomove').find('a').length>0) {
-            $.get($('.roomItemActions').children().filter(function(index){if ($(this).attr("onmouseover").indexOf("Coffee!")>=0) return true;}).find("a").attr("href"),function(data) {});
-        }
+        $.get($('.roomItemActions').children().filter(function(index){ return $(this).attr("onmouseover").indexOf("Coffee!")>=0; }).find("a").attr("href"),function(data) {
+            // Callback for GET response
+            div_response.html(data);
+            if (div_response.find('.cdChatPack:first').children('.cdContent').find(".what_happened:contains("+$('.who').html().trim()+' primes the machine.'+")").length>0) { // Check for log
+                // Coffee retrieved by me in this cycle
+                $('.cdFavWall .unit').each(function(){
+                    if($(this).find(".mainsaid").html().search(/(coffee|кофе|food|еда|refectory|кухня)/i) != -1){
+                        Main.ajaxChat("/wallReply?k=" + $(this).data("k") + "&msg=" + encodeURIComponent(div_response.find('.cycletime').text().trim().replace(/Day (\d{1,2}) - Cycle (\d)/,"D$1C$2")+": Coffee retrieved"));
+                        return false;
+                    }
+                });
+                console.log(div_response.find('.cycletime').text()+': сoffee retrieved');
+            }
+        });
     }
-    setTimeout(RetrieveCoffee,7200000);
+    // Schedule to run again after cycle change
+    var re=/((\d{1,2})h)?(\d{2})m(\d{2})s/;
+    var str_timeToCycle=div_response.find('.cdTimeStamp').text();
+    var timeToCycle = str_timeToCycle.replace(re,"$2")*3600+str_timeToCycle.replace(re,"$3")*60+str_timeToCycle.replace(re,"$4")*1;
+    setTimeout(RetrieveCoffee,(timeToCycle+5)*1000);
 }
 
-console.log('Кофедоставатель будет пытаться доставать кофе каждые два часа, начиная со следующего цикла');
-RetrieveCoffee();
+var div_response = jQuery('<div/>', {id: 'mush_coffee_handler'});
+console.log('Coffee retriever activated');
+setTimeout(RetrieveCoffee,Main.tData.timeToCycle - (new Date()).getTime() + Main.tData.clientNow.getTime() + 5000);
