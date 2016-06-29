bc35d4481150e4beb960a606b1d9f2f8ae45ab57
Yuriy Tkachenko <yuri.tam.tkachenko@gmail.com>
2016-06-29 10:29:34 +0400

    Fix for opening links in new tab

--- Linkify.user.js
+++ Linkify.user.js
@@ -13,5 +13,5 @@
 $(".mainsaid p").add(".reply p").add(".talks p").html(function() {
     var urlPattern = /\b(?:https?|ftp):\/\/[a-z0-9-+&@#\/%?=~_|!:,.;]*[a-z0-9-+&@#\/%=~_|]/gim;
     var pseudoUrlPattern = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
-    return $(this).html().replace(urlPattern, '<a href="$&" target-"_blank">$&</a>').replace(pseudoUrlPattern, '$1<a href="http://$2">$2</a>');
+    return $(this).html().replace(urlPattern, '<a href="$&" target="_blank">$&</a>').replace(pseudoUrlPattern, '$1<a href="http://$2">$2</a>');
 });
